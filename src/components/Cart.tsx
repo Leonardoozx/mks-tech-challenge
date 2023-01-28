09dfe666b254dbebc2df412c1538d6996baaaa2f
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-28 11:46:12 -0300

    feat: cart component

---
+++ src/components/Cart.tsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import styled from "styled-components";
+
+import cartIcon from '../images/cartIcon.svg';
+
+const CartIconSection = styled.button`
+  display: flex;
+  width: 52px;
+  height: 26px;
+  align-items: center;
+  padding: 5px;
+  gap: 8px;
+  border-radius: 8px;
+`;
+
+export default function Cart() {
+  return (
+    <CartIconSection>
+      <Image src={cartIcon} alt="cart icon" width={15}></Image>
+      <span style={{ color: 'black' }}>0</span>
+    </CartIconSection>
+  )
+}
