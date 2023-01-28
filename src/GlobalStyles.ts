3f4c1216624002dff27e577dd1175c3f79ebeb2e
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-28 11:44:13 -0300

    style: adds globalStyles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,36 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+* { 
+  margin: 0;
+  padding: 0;
+}
+
+*,
+*::before,
+*::after {
+  font-style: normal;
+  box-sizing: inherit;
+}
+
+html {
+  box-sizing: border-box;
+  position: relative;
+}
+
+body {
+  font-family: 'Lato', sans-serif;
+  background-color: #E5E5E5;
+}
+
+p {
+  font-size: 16;
+}
+
+section {
+  border-radius: 8px;
+  background-color: white;
+}
+`;
+
+export default GlobalStyles;
