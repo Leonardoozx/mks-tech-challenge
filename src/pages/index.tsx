d9365af491854b67449dd8f3daba171275fadb18
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-29 11:43:20 -0300

    feat: implements ItemsCard component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,19 @@ import { useAppDispatch, useAppSelector } from "@/store/hooks"
 import { fetchItems } from "@/store/reducers/stockItemsReducer"
 import { useEffect, useState } from "react"
 import { selectItems } from "@/store/reducers/stockItemsReducer"
+import ItemsCard from "@/components/ItemsCard"
+import styled from "styled-components"
+
+const ItemsContainer = styled.div`
+  width: 90%;
+  margin: 100px auto;
+  border-radius: 8px;
+  margin: 40px auto 0 auto;
+`;
 
 export default function Home() {
   const dispatch = useAppDispatch();
-
   const [ isLoading, setIsLoading ] = useState(true);
-
   const items = useAppSelector(selectItems);
 
   useEffect(() => {
@@ -21,40 +28,24 @@ export default function Home() {
       sortBy: 'id',
       orderBy: 'DESC'
     };
-
     dispatch(fetchItems({ ...payload }))
-
     if (items.stockItems) return setIsLoading(false)
-
   }, [ dispatch, items ])
 
   return (
     <div>
-      <HeadPageInfo />
-      <Header />
       {
         isLoading ? (
           <SkeletonLoading />
         ) :
           (
-            <>
-              {items?.stockItems.map((item, index) => (
-                <div key={index}>
-                  <p>{item.id}</p>
-                  <p>{item.brand}</p>
-                  <p>{item.createdAt}</p>
-                  <p>{item.description}</p>
-                  <p>{item.name}</p>
-                  <p>{item.photo}</p>
-                  <p>{item.price}</p>
-                  <p>{item.updatedAt}</p>
-                </div>
+            <ItemsContainer>
+              {items?.stockItems.map((item) => (
+                <ItemsCard key={item.id} item={item} />
               ))}
-            </>
+            </ItemsContainer>
           )
       }
-
-      
     </div>
   )
 }
