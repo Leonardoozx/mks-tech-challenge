d85b9fcae3bcc4a24be97d435b7869f0d968ae6c
Leonardoozx <leonardo2025maldarck@gmail.com>
2023-01-30 18:47:21 -0300

    style: adds `Montserrat` font-family

--- src/GlobalStyles.ts
+++ src/GlobalStyles.ts
@@ -4,6 +4,8 @@ const GlobalStyles = createGlobalStyle`
 * { 
   margin: 0;
   padding: 0;
+  font-family: 'Montserrat', sans-serif;
+  font-style: normal;
 }
 
 *,
@@ -21,6 +23,8 @@ html {
 body {
   font-family: 'Lato', sans-serif;
   background-color: #E5E5E5;
+  height: 100%;
+  position: relative;
 }
 
 p {
